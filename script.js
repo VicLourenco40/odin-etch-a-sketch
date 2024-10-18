e6153fdf0d81cc71199e41cb22ff7deab7e8a40e
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-18 10:26:27 +0100

    Create grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const GRID_CONTAINER_SIZE = 640;
+
+const gridContainer = document.querySelector('.grid-container');
+
+gridContainer.style.width = GRID_CONTAINER_SIZE + 'px';
+gridContainer.style.height = GRID_CONTAINER_SIZE + 'px';
+
+function createGrid(cellAmount = 16) {
+    const gridCellSize = GRID_CONTAINER_SIZE / cellAmount;
+
+    for (let i = 0; i < cellAmount ** 2; i++) {
+        const gridCell = document.createElement('div');
+
+        gridCell.style.width = gridCellSize + 'px';
+        gridCell.style.height = gridCellSize + 'px';
+        gridCell.classList.add('border');
+
+        gridContainer.appendChild(gridCell);
+    }
+}
+
+createGrid();
