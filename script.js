b01c418f4b2e37de7cac6246cb307a7ff6c29825
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-18 11:33:39 +0100

    Add change grid size feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const GRID_CONTAINER_SIZE = 640;
 
 const gridContainer = document.querySelector('.grid-container');
+const buttonGridSize = document.querySelector('.button-grid-size');
 
 gridContainer.style.width = GRID_CONTAINER_SIZE + 'px';
 gridContainer.style.height = GRID_CONTAINER_SIZE + 'px';
@@ -8,12 +9,15 @@ gridContainer.style.height = GRID_CONTAINER_SIZE + 'px';
 function createGrid(cellAmount = 16) {
     const gridCellSize = GRID_CONTAINER_SIZE / cellAmount;
 
+    while(gridContainer.lastChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+
     for (let i = 0; i < cellAmount ** 2; i++) {
         const gridCell = document.createElement('div');
 
         gridCell.style.width = gridCellSize + 'px';
         gridCell.style.height = gridCellSize + 'px';
-        gridCell.classList.add('border');
 
         gridContainer.appendChild(gridCell);
     }
@@ -26,5 +30,11 @@ function createGrid(cellAmount = 16) {
         event.target.style.backgroundColor = 'black';
     });
 }
+
+buttonGridSize.addEventListener('click', () => {
+    const cellAmount = parseInt(prompt('Grid size', 16));
+
+    createGrid(cellAmount);
+})
 
 createGrid();
